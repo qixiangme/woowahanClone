4ec4fabfbc59c8ba24033a49a0172d8240d2d535
qixiangme <erang903@khu.ac.kr>
2025-04-29 10:42:58 +0900

    fetch baseurl

--- src/Home.jsx
+++ src/Home.jsx
@@ -27,7 +27,7 @@ function Home() {
 
   // 텍스트 동적으로 렌더링
   useEffect(() => {
-    fetch("/data.json")
+    fetch("woowahanclone/data.json")
       .then((response) => response.json())
       .then((data) => {
         const paragraph2 = data[activeIndex].paragraph2 || [];
